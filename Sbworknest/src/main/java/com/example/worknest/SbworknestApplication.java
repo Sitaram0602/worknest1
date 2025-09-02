2c0dee0a97e049c39a146a5046492ca25f973519
Sitaram0602 <chowdarys.590@gmail.com>
2025-09-02 08:17:43 +0530

    Add files via upload

---
+++ Sbworknest/src/main/java/com/example/worknest/SbworknestApplication.java
@@ -0,0 +1,13 @@
+package com.example.worknest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbworknestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbworknestApplication.class, args);
+	}
+
+}
